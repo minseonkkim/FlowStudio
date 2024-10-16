308d102be76cba15cdfa3a559cee5f8063b3da0c
JaewonChoi <wefhio1985@gmail.com>
2024-10-16 17:53:59 +0900

    [#2] Config: 백엔드 초기 세팅

---
+++ backend/flowstudio/src/test/java/com/ssafy/flowstudio/FlowStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.flowstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ backend/flowstudio/src/test/java/com/ssafy/flowstudio/support/IntegrationTestSupport.java
@@ -0,0 +1,8 @@
+package com.ssafy.flowstudio.support;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+
+}
