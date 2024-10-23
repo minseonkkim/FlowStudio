3eb4c65e08ff73f9c8b4eb96d56097170bb00302
고도연 <focus0007@naver.com>
2024-10-23 09:59:39 +0900

    design: 로그인 화면 디자인 중

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import Logo from '../../assets/common/logo.png'
+import Image from 'next/image'
+import { VscGithub } from "@react-icons/all-files/vsc/VscGithub";
+import { FaAngleLeft } from "@react-icons/all-files/fa/FaAngleLeft";
 const page = () => {
   return (
-    <div>로그인</div>
+    <>
+    <Image src={Logo} alt="logo" className="w-32 h-32 object-cover"/>
+    <div className='text-4xl font-semibold'>FlowStudio에 오신 것을 환영합니다.</div>
+    <div className='text-[#757575] text-1g'>손쉽게 맞춤형 챗봇을 생성, 배포, 관리해보세요.</div>
+    <VscGithub/>
+    <FaAngleLeft/>
+    </>
   )
 }
 
