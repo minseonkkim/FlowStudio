b2672eb29ccb6a3ab7bfae8bedb224c436dde1e0
JaeHunJang <wuddlaa@gmail.com>
2024-11-05 01:14:39 +0900

    [#25] Refactor: KnowledgeResponse createAt type change
    LocalDateTime to String

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/rag/response/KnowledgeResponse.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/rag/response/KnowledgeResponse.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 @Getter
 public class KnowledgeResponse {
@@ -12,14 +14,14 @@ public class KnowledgeResponse {
     private final Long knowledgeId;
     private final String title;
     private final Boolean isPublic;
-    private final LocalDateTime createAt;
+    private final String createAt;
 
     @Builder
     public KnowledgeResponse(Long knowledgeId, String title, Boolean isPublic, LocalDateTime createAt) {
         this.knowledgeId = knowledgeId;
         this.title = title;
         this.isPublic = isPublic;
-        this.createAt = createAt;
+        this.createAt = createAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
     public static KnowledgeResponse from(Knowledge knowledge) {
