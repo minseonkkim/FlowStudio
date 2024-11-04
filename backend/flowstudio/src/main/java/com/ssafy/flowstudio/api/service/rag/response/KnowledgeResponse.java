da28a0cf7a5e90b9e73300d1687d524ca62dad21
JaeHunJang <wuddlaa@gmail.com>
2024-11-04 14:42:39 +0900

    [#25] Rename: Package change KnowledgeResponse

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/rag/response/KnowledgeResponse.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/rag/response/KnowledgeResponse.java
@@ -1,26 +1,33 @@
-package com.ssafy.flowstudio.api.service.node.response;
+package com.ssafy.flowstudio.api.service.rag.response;
 
 import com.ssafy.flowstudio.domain.knowledge.entity.Knowledge;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @Getter
 public class KnowledgeResponse {
 
     private final Long knowledgeId;
     private final String title;
+    private final Boolean isPublic;
+    private final LocalDateTime createAt;
 
     @Builder
-    private KnowledgeResponse(Long knowledgeId, String title) {
+    public KnowledgeResponse(Long knowledgeId, String title, Boolean isPublic, LocalDateTime createAt) {
         this.knowledgeId = knowledgeId;
         this.title = title;
+        this.isPublic = isPublic;
+        this.createAt = createAt;
     }
 
     public static KnowledgeResponse from(Knowledge knowledge) {
         return KnowledgeResponse.builder()
                 .knowledgeId(knowledge.getId())
                 .title(knowledge.getTitle())
+                .isPublic(knowledge.isPublic())
+                .createAt(knowledge.getCreatedAt())
                 .build();
     }
-
 }
