e25f66e5a84016c6555b4b8c4071df056a634ba5
JaeHunJang <wuddlaa@gmail.com>
2024-11-05 01:17:01 +0900

    [#25] Feature: KnowledgeListResponse add field isPublic
    isPublic add

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/rag/response/KnowledgeListResponse.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/rag/response/KnowledgeListResponse.java
@@ -10,17 +10,20 @@ import java.util.List;
 public class KnowledgeListResponse {
     private final Long knowledgeId;
     private final String title;
+    private final Boolean isPublic;
 
     @Builder
-    private KnowledgeListResponse(Long knowledgeId, String title) {
+    private KnowledgeListResponse(Long knowledgeId, String title, Boolean isPublic) {
         this.knowledgeId = knowledgeId;
         this.title = title;
+        this.isPublic = isPublic;
     }
 
     public static KnowledgeListResponse from(Knowledge knowledge) {
         return KnowledgeListResponse.builder()
                 .knowledgeId(knowledge.getId())
                 .title(knowledge.getTitle())
+                .isPublic(knowledge.isPublic())
                 .build();
     }
 
