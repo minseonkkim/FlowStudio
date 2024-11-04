1f381bb3c2d9d5def4f5b8b5728ef0aece56f583
garamgim <garamgim@gmail.com>
2024-11-04 21:57:42 +0900

    [#34] Feature: 질문 엔티티 생성

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/node/executor/QuestionClassifierExecutor.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/node/executor/QuestionClassifierExecutor.java
@@ -1,31 +1,53 @@
 package com.ssafy.flowstudio.api.service.node.executor;
 
+import com.ssafy.flowstudio.common.secret.SecretKeyProperties;
 import com.ssafy.flowstudio.domain.chat.entity.Chat;
 import com.ssafy.flowstudio.domain.node.entity.Node;
 import com.ssafy.flowstudio.domain.node.entity.NodeType;
 import com.ssafy.flowstudio.domain.node.entity.QuestionClassifier;
-import com.ssafy.flowstudio.domain.user.entity.ApiKey;
-import com.ssafy.flowstudio.domain.user.entity.User;
-import lombok.RequiredArgsConstructor;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_3_5_TURBO;
 
 @Component
 public class QuestionClassifierExecutor extends NodeExecutor {
 
-    public QuestionClassifierExecutor(RedisTemplate<String, Object> redisTemplate) {
+    private final SecretKeyProperties secretKeyProperties;
+
+    public QuestionClassifierExecutor(RedisTemplate<String, Object> redisTemplate, SecretKeyProperties secretKeyProperties) {
         super(redisTemplate);
+        this.secretKeyProperties = secretKeyProperties;
     }
 
     // TODO : model 데이터 넣기 + param_list 작성
-    // TODO : user 정보 필요 (API KEY)
     @Override
     public void execute(Node node, Chat chat) {
         QuestionClassifier questionClassifierNode = (QuestionClassifier) node;
-        User user = chat.getUser();
-        ApiKey apiKey = user.getApiKey();
         String classList = questionClassifierNode.getClassList();
         String modelParamList = questionClassifierNode.getModelParamList();
+        ChatLanguageModel model = OpenAiChatModel.builder()
+                .apiKey(secretKeyProperties.getOpenAi())
+                .modelName(GPT_3_5_TURBO)
+                .build();
+
+        SystemMessage systemMessage = new SystemMessage("system message");
+        UserMessage userMessage = new UserMessage("user message");
+
+        List<ChatMessage> messageList = new ArrayList<>();
+
+        messageList.add(systemMessage);
+        messageList.add(userMessage);
+
+
         System.out.println("QuestionClassifierExecutor");
     }
 
--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/domain/node/entity/QuestionClass.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/domain/node/entity/QuestionClass.java
@@ -1,2 +1,30 @@
-package com.ssafy.flowstudio.domain.node.entity;public class QuestionClass {
+package com.ssafy.flowstudio.domain.node.entity;
+
+import com.ssafy.flowstudio.domain.chatflow.entity.ChatFlow;
+import com.ssafy.flowstudio.domain.edge.entity.Edge;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class QuestionClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "question_class_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "edge_id")
+    private Edge edge;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_classifier_id")
+    private QuestionClassifier questionClassifier;
 }
