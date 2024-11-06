d72d7c9473d96fe2b5b9197dbe4aa56755bd78cf
JaewonChoi <wefhio1985@gmail.com>
2024-11-06 10:27:22 +0900

    [#38] Refactor: publisher함수를 NodeExecutor로 이동

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/node/event/NodeEventListener.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/api/service/node/event/NodeEventListener.java
@@ -1,24 +1,27 @@
 package com.ssafy.flowstudio.api.service.node.event;
 
-import com.ssafy.flowstudio.api.service.node.NodeVisitorImpl;
 import com.ssafy.flowstudio.domain.chat.entity.Chat;
 import com.ssafy.flowstudio.domain.node.entity.Node;
+import com.ssafy.flowstudio.domain.node.entity.NodeVisitor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NodeEventListener {
 
-    private final NodeVisitorImpl visitor;
+    private final NodeVisitor visitor;
 
     @EventListener
     public void handleNodeEvent(NodeEvent event) {
-
         Node targetNode = event.getTargetNode();
         Chat chat = event.getChat();
 
+        log.info("노드 실행 타입: {}", targetNode.getType());
         targetNode.accept(visitor, chat);
     }
+
 }
