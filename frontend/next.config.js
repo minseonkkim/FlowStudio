287e525a211f85f2b5777fd734f7dea093cc0f76
고도연 <focus0007@naver.com>
2024-11-08 16:13:33 +0900

    fix: 오류 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     output: 'standalone',
     images: {
-        domains: ['lh3.googleusercontent.com', 'k.kakaocdn.net', 'avatars.githubusercontent.com'],
+        domains: ['lh3.googleusercontent.com', 'k.kakaocdn.net', 'avatars.githubusercontent.com','img1.kakaocdn.net'],
     },
 }
