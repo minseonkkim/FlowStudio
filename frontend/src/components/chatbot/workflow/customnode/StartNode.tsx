83458d8a8bf2b7f33f73eba25a99589deb5c0b74
surina125 <jhyunsoo1125@gmail.com>
2024-11-10 03:45:46 +0900

    [#51] Feature: 노드 삭제 api연동

--- frontend/src/components/chatbot/workflow/customnode/StartNode.tsx
+++ frontend/src/components/chatbot/workflow/customnode/StartNode.tsx
@@ -13,7 +13,7 @@ interface StartNodeProps {
 }
 
 export default function StartNode({ data, selected }: StartNodeProps) {
-  const { onDelete } = data;
+  // const { onDelete } = data;
 
   return (
     <>
@@ -28,12 +28,12 @@ export default function StartNode({ data, selected }: StartNodeProps) {
               <IoPlay className="text-[#95C447] size-3" />
               <div className="text-[11px] font-semibold">시작</div>
             </div>
-            {selected && (
+            {/* {selected && (
               <MdDelete
                 className="cursor-pointer text-[#95C447] hover:text-[#6F9335] size-3.5"
                 onClick={onDelete}
               />
-            )}
+            )} */}
           </div>
           {typeof data.maxChars === "number" && !isNaN(data.maxChars) && (
             <div className="flex flex-col gap-0.5 text-[8px]">
