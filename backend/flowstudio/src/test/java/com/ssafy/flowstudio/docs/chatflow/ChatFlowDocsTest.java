8f004e298fb8155e1196e0e11eaaef4e375dcd92
JaewonChoi <wefhio1985@gmail.com>
2024-11-12 09:05:54 +0900

    [#20] Docs: 예시 챗플로우 생성 docs 작성

--- backend/flowstudio/src/test/java/com/ssafy/flowstudio/docs/chatflow/ChatFlowDocsTest.java
+++ backend/flowstudio/src/test/java/com/ssafy/flowstudio/docs/chatflow/ChatFlowDocsTest.java
@@ -533,4 +533,206 @@ public class ChatFlowDocsTest extends RestDocsSupport {
                                 .build())));
     }
 
+
+
+    @DisplayName("예시 챗플로우를 생성한다")
+    @Test
+    void createExampleChatFlow() throws Exception {
+        EdgeResponse edge1 = EdgeResponse.builder()
+                .edgeId(1L)
+                .sourceNodeId(1L)
+                .targetNodeId(2L)
+                .build();
+
+        EdgeResponse edge2 = EdgeResponse.builder()
+                .edgeId(2L)
+                .sourceNodeId(2L)
+                .targetNodeId(3L)
+                .sourceConditionId(1L)
+                .build();
+
+        EdgeResponse edge3 = EdgeResponse.builder()
+                .edgeId(3L)
+                .sourceNodeId(3L)
+                .targetNodeId(4L)
+                .build();
+
+        EdgeResponse edge4 = EdgeResponse.builder()
+                .edgeId(4L)
+                .sourceNodeId(4L)
+                .targetNodeId(5L)
+                .build();
+
+        CoordinateResponse coordinate = CoordinateResponse.builder()
+                .x(100)
+                .y(100)
+                .build();
+
+        NodeResponse node1 = StartResponse.builder()
+                .nodeId(1L)
+                .name("Start")
+                .type(NodeType.START)
+                .coordinate(coordinate)
+                .inputEdges(List.of(edge1))
+                .outputEdges(List.of())
+                .maxLength(10)
+                .build();
+
+        NodeResponse node2 = QuestionClassifierResponse.builder()
+                .nodeId(2L)
+                .name("QuestionClassifier")
+                .type(NodeType.QUESTION_CLASSIFIER)
+                .coordinate(coordinate)
+                .inputEdges(List.of(edge1))
+                .outputEdges(List.of(edge2))
+                .build();
+
+        KnowledgeResponse knowledge = KnowledgeResponse.builder()
+                .knowledgeId(1L)
+                .title("title")
+                .isPublic(true)
+                .createdAt(LocalDateTime.of(2021, 1, 1, 0, 0))
+                .totalToken(10)
+                .build();
+
+        NodeResponse node3 = RetrieverResponse.builder()
+                .nodeId(3L)
+                .name("Retriever")
+                .type(NodeType.RETRIEVER)
+                .coordinate(coordinate)
+                .knowledge(knowledge)
+                .intervalTime(10)
+                .topK(10)
+                .scoreThreshold(0.5f)
+                .query("query")
+                .inputEdges(List.of(edge2))
+                .outputEdges(List.of(edge3))
+                .build();
+
+        NodeResponse node4 = LlmResponse.builder()
+                .nodeId(4L)
+                .name("LLM")
+                .type(NodeType.LLM)
+                .promptSystem("promptSystem")
+                .promptUser("promptUser")
+                .coordinate(coordinate)
+                .inputEdges(List.of(edge3))
+                .outputEdges(List.of(edge4))
+                .build();
+
+        NodeResponse node5 = AnswerResponse.builder()
+                .nodeId(5L)
+                .name("Answer")
+                .type(NodeType.ANSWER)
+                .coordinate(coordinate)
+                .outputEdges(List.of())
+                .inputEdges(List.of(edge4))
+                .outputMessage("outputMessage")
+                .build();
+
+        ChatFlowResponse response = ChatFlowResponse.builder()
+                .chatFlowId(1L)
+                .title("title")
+                .nodes(List.of(node1, node2, node3, node4, node5))
+                .edges(List.of(edge1, edge2, edge3, edge4))
+                .build();
+
+        given(chatFlowService.createExampleChatFlow(any(User.class)))
+                .willReturn(response);
+
+        // when
+        ResultActions perform = mockMvc.perform(
+                post("/api/v1/chat-flows/example", 1L)
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        perform
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andDo(document("create-chatflow-example",
+                        preprocessResponse(prettyPrint()),
+                        resource(ResourceSnippetParameters.builder()
+                                .tag("ChatFlow")
+                                .summary("챗플로우 예시 생성")
+                                .responseFields(
+                                        fieldWithPath("code").type(JsonFieldType.NUMBER)
+                                                .description("코드"),
+                                        fieldWithPath("status").type(JsonFieldType.STRING)
+                                                .description("상태"),
+                                        fieldWithPath("message").type(JsonFieldType.STRING)
+                                                .description("메시지"),
+                                        fieldWithPath("data.chatFlowId").type(JsonFieldType.NUMBER)
+                                                .description("챗플로우 아이디"),
+                                        fieldWithPath("data.title").type(JsonFieldType.STRING)
+                                                .description("챗플로우 제목"),
+                                        fieldWithPath("data.nodes[].nodeId").type(JsonFieldType.NUMBER)
+                                                .description("노드 아이디"),
+                                        fieldWithPath("data.nodes[].name").type(JsonFieldType.STRING)
+                                                .description("노드 이름"),
+                                        fieldWithPath("data.nodes[].type").type(JsonFieldType.STRING)
+                                                .description("노드 타입"),
+                                        fieldWithPath("data.nodes[].questionClasses").optional().type(JsonFieldType.STRING)
+                                                .description("노드 타입"),
+                                        fieldWithPath("data.nodes[].intervalTime").optional().type(JsonFieldType.NUMBER)
+                                                .description("Retriever노드 intervalTime"),
+                                        fieldWithPath("data.nodes[].topK").optional().type(JsonFieldType.NUMBER)
+                                                .description("Retriever노드 topK"),
+                                        fieldWithPath("data.nodes[].scoreThreshold").optional().type(JsonFieldType.NUMBER)
+                                                .description("Retriever노드 scoreThreshold"),
+                                        fieldWithPath("data.nodes[].query").optional().type(JsonFieldType.STRING)
+                                                .description("Retriever노드 query"),
+                                        fieldWithPath("data.nodes[].knowledge.knowledgeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("Retriever노드 지식 아이디"),
+                                        fieldWithPath("data.nodes[].knowledge.title").optional().type(JsonFieldType.STRING)
+                                                .description("Retriever노드 지식 제목"),
+                                        fieldWithPath("data.nodes[].knowledge.isPublic").optional().type(JsonFieldType.BOOLEAN)
+                                                .description("Retriever노드 지식 공개여부"),
+                                        fieldWithPath("data.nodes[].knowledge.createdAt").optional().type(JsonFieldType.STRING)
+                                                .description("Retriever노드 지식 생성일"),
+                                        fieldWithPath("data.nodes[].knowledge.totalToken").optional().type(JsonFieldType.NUMBER)
+                                                .description("Retriever노드 지식 토큰 수"),
+                                        fieldWithPath("data.nodes[].promptSystem").optional().type(JsonFieldType.STRING)
+                                                .description("LLM노드 시스템프롬프트"),
+                                        fieldWithPath("data.nodes[].promptUser").optional().type(JsonFieldType.STRING)
+                                                .description("LLM노드 유저프롬프트"),
+                                        fieldWithPath("data.nodes[].outputMessage").optional().type(JsonFieldType.STRING)
+                                                .description("Answer노드 출력메시지"),
+                                        fieldWithPath("data.nodes[].maxLength").optional().type(JsonFieldType.NUMBER)
+                                                .description("Start 노드 입력 최대길이"),
+                                        fieldWithPath("data.nodes[].coordinate.x").type(JsonFieldType.NUMBER)
+                                                .description("노드 X좌표"),
+                                        fieldWithPath("data.nodes[].coordinate.y").type(JsonFieldType.NUMBER)
+                                                .description("노드 Y좌표"),
+                                        fieldWithPath("data.nodes[].outputEdges[].edgeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("output 간선 아이디"),
+                                        fieldWithPath("data.nodes[].outputEdges").optional().type(JsonFieldType.ARRAY)
+                                                .description("output 간선"),
+                                        fieldWithPath("data.nodes[].outputEdges[].sourceNodeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("output 간선 출발노드 아이디"),
+                                        fieldWithPath("data.nodes[].outputEdges[].targetNodeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("output 간선 도착노드 아이디"),
+                                        fieldWithPath("data.nodes[].outputEdges[].sourceConditionId").optional().type(JsonFieldType.NUMBER)
+                                                .description("input 간선 시작 조건 아이디"),
+                                        fieldWithPath("data.nodes[].inputEdges[].edgeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("input 간선 아이디"),
+                                        fieldWithPath("data.nodes[].inputEdges[].sourceNodeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("input 간선 출발노드 아이디"),
+                                        fieldWithPath("data.nodes[].inputEdges[].targetNodeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("input 간선 도착노드 아이디"),
+                                        fieldWithPath("data.nodes[].inputEdges[].sourceConditionId").optional().type(JsonFieldType.NUMBER)
+                                                .description("input 간선 시작 조건 아이디"),
+                                        fieldWithPath("data.edges[].edgeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("edge 아이디"),
+                                        fieldWithPath("data.edges[].sourceNodeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("시작 노드"),
+                                        fieldWithPath("data.edges[].targetNodeId").optional().type(JsonFieldType.NUMBER)
+                                                .description("도착 노드"),
+                                        fieldWithPath("data.edges[].sourceConditionId").optional().type(JsonFieldType.NUMBER)
+                                                .description("조건 아이디")
+                                )
+                                .build()
+                        )
+                ));
+    }
+
 }
