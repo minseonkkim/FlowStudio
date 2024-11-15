f08e59ec0c7b1462d6e11d1fac7f4058683acdce
minseonkkim <minsun9856@gmail.com>
2024-11-16 00:16:22 +0900

    [#65] Feature: default chat 반응형 구현, 프로필사진 로컬스토리지 저장

--- frontend/src/app/auth/callback/page.tsx
+++ frontend/src/app/auth/callback/page.tsx
@@ -1,22 +1,39 @@
 'use client'
 import { Suspense, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { getUserInfo } from '@/api/profile';
+import { useQuery } from '@tanstack/react-query';
+import { useRecoilState } from 'recoil';
+import { profileImageAtom } from '@/store/profileAtoms';
+import { UserInfo } from '@/types/profile';
 
 function CallbackContent() {
   const router = useRouter();
   const searchParams = useSearchParams();
+  const [profileImg, setProfileImg] = useRecoilState(profileImageAtom);
+
+  const { isLoading: isUserInfoLoading, isError: isUserInfoError, error: userInfoError, data: userInfo } = useQuery<UserInfo>({
+    queryKey: ['userInfo'],
+    queryFn: getUserInfo,
+  });
 
   useEffect(() => {
     const token = searchParams.get('accessToken');
 
     if (token) {
       localStorage.setItem('accessToken', token);
+
+      if (userInfo) {
+        setProfileImg(userInfo.profileImage);
+        localStorage.setItem('profileImage', userInfo.profileImage); // Save to local storage
+      }
+
       router.push('/');
     } else {
       console.error('accessToken not found in query parameters');
       router.push('/login');
     }
-  }, [router, searchParams]);
+  }, [router, searchParams, userInfo]);
 
   return <p>로그인 중입니다...</p>;
 }
