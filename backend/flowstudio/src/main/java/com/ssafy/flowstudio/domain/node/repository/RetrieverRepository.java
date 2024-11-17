a9dfdcb2d8cc30d0269bcbe6a6ba5d9a03fc6f66
JaewonChoi <wefhio1985@gmail.com>
2024-11-17 19:35:03 +0900

    [#41] Fix: 지식 삭제시 지식을 참조하고 있는 지식검색기에서 지식 제거

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/domain/node/repository/RetrieverRepository.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/domain/node/repository/RetrieverRepository.java
@@ -5,10 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RetrieverRepository extends JpaRepository<Retriever, Long> {
     @Query("select r from Retriever r right join Node n on r.id = n.id where n.id = :id")
     Optional<Retriever> findById(Long id);
+
+    @Query("select r from Retriever r where r.knowledge.id = :knowledgeId")
+    List<Retriever> findByKnowledgeId(Long knowledgeId);
 }
