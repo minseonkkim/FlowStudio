4471c1661faab239aad1c84c4f1dd1ae8304a602
garamgim <garamgim@gmail.com>
2024-11-18 16:23:37 +0900

    [#79] Fix: 노드 복제 시 메시지 파싱 에러 해결

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/domain/node/factory/copy/NodeCopyFactory.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/domain/node/factory/copy/NodeCopyFactory.java
@@ -10,12 +10,4 @@ public abstract class NodeCopyFactory {
     public Node copyNode(Node node, ChatFlow clonedChatFlow, Knowledge knowledge) {
         return copyNode(node, clonedChatFlow);
     }
-
-    public Node copyNode(Node node, ChatFlow clonedChatFlow, String clonedOutputMessage) {
-        return copyNode(node, clonedChatFlow);
-    }
-
-    public Node copyNode(Node node, ChatFlow clonedChatFlow, String clonedPromptSystem, String clonedPromptUser) {
-        return copyNode(node, clonedChatFlow);
-    }
 }
