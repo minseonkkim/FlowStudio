e646e6450c09c5b5a823fe467fcd78d157e7ff76
JaewonChoi <wefhio1985@gmail.com>
2024-11-18 16:23:13 +0900

    [#20] Fix: 챗봇 발행시 유저의 apikey fetchjoin

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/domain/user/repository/UserRepository.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/domain/user/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.flowstudio.domain.user.repository;
 
 import com.ssafy.flowstudio.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -9,4 +10,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     boolean existsByNickname(String nickname);
+
+    @Query("select u from User u join fetch u.apiKey where u.id = :id")
+    Optional<User> findByIdWithApiKey(Long id);
 }
