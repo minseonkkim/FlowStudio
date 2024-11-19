cd9f765bbd648e7acdb01952bc4b4d6c9eee8681
minseonkkim <minsun9856@gmail.com>
2024-11-19 17:23:34 +0900

    [#87] Fix: 최신순 정렬 안되는 문제 해결

--- frontend/src/app/chatbots/page.tsx
+++ frontend/src/app/chatbots/page.tsx
@@ -211,7 +211,7 @@ export default function Page() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full gap-4">
-        {filteredChatFlows.reverse().slice(0, itemsToLoad).map((bot) => (
+        {filteredChatFlows.slice().reverse().slice(0, itemsToLoad).map((bot) => (
           <PopularChatbotCard
             key={bot.chatFlowId}
             chatbotId={bot.chatFlowId}
