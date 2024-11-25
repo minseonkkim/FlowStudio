ebf5ff9dd32589c274e3532ba1639d0301ff305b
JaeHunJang <wuddlaa@gmail.com>
2024-11-25 13:21:58 +0900

    [#99] Refactor: PublishService publish chatFlow checked apikey and executable

--- backend/flowstudio/src/main/java/com/ssafy/flowstudio/common/exception/ErrorCode.java
+++ backend/flowstudio/src/main/java/com/ssafy/flowstudio/common/exception/ErrorCode.java
@@ -27,6 +27,7 @@ public enum ErrorCode {
     UPLOADED_CHAT_FLOW_CANNOT_BE_SHARED(3001, HttpStatus.NOT_FOUND, "게시된 상태의 챗플로우는 업로드할 수 없습니다"),
     CHAT_FLOW_NOT_PUBLISHED(3002, HttpStatus.SERVICE_UNAVAILABLE, "발행되지 않은 챗플로우입니다."),
     CHAT_FLOW_CYCLE_DETECTED(3003, HttpStatus.BAD_REQUEST, "챗플로우 내에 사이클이 감지되었습니다."),
+    CHAT_FLOW_NOT_RUNNABLE(3004, HttpStatus.SERVICE_UNAVAILABLE, "실행할 수 없는 챗플로우 입니다."),
 
     // Node
     INVALID_NODE_TYPE(4000, HttpStatus.BAD_REQUEST, "지원되지 않는 노드 타입입니다."),
