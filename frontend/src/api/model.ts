2c1097d1c6e0dda5bf93b84e5b01c5f8dd7b64d6
minseonkkim <minsun9856@gmail.com>
2024-11-27 10:47:14 +0900

    [#106] Refactor: 콘솔문 제거

--- frontend/src/api/model.ts
+++ frontend/src/api/model.ts
@@ -5,7 +5,6 @@ export async function getModels() {
   try {
     const response = await axiosInstance.get('chat-flows/nodes/models');
     if (response.status === 200) {
-      console.log('모델 목록', response.data.data)
       return response.data.data;
     } else {
       throw new Error('Failed to get models');
